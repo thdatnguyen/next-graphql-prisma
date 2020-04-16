3ae23ac9853695d30979dd12e9e8a6b00e84e866
Tada Nguyen <tada@splashinteractive.com.vn>
2020-04-16 15:47:46 +0700

    update schema and config url path for production mode

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -1,6 +1,6 @@
 import withApollo from 'next-with-apollo';
 import ApolloClient from 'apollo-boost';
-import { endpoint } from '../config';
+import { endpoint, prodEndpoint } from '../config';
 import { LOCAL_STATE_QUERY } from '../components/Cart.component';
 function createClient({ headers }) {
   return new ApolloClient({
